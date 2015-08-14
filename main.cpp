bcb276c0e51a960a2545c6ca977d229962f749af
James Chow <James@169-231-83-241.wireless.ucsb.edu>
2015-08-13 18:08:59 -0700

    Fixed stuff

--- main.cpp
+++ main.cpp
@@ -11,7 +11,10 @@ void collide(Object& a, Object& b);
 void movement(Object &b);
 void hit_wall(Object& b);
 bool approx_equal_pos(Object& a, Object& b);
+
+int num_collisions;
 double length;
+
 
 int main(){
     
@@ -20,63 +23,60 @@ int main(){
     Object * wall = new Object;
     
     wall->position = 0;
-    wall-> velocity = 0;
+    wall->velocity = 0;
     
-    /*
-    cout << "Enter initial velocity of block:" << endl;
-    cin >> block->velocity;
-    cout << "Enter mass of block and ball" << endl;
-    cin >> block->mass >> ball->mass;
-    cout << "Enter initial distance from wall" << endl;
-    cin >> length;
-    ball->position = -1*length;
-    block->position = -1 * length;
-    */
     
     // testing movement and collisions
-    length = 50;
-    ball->position = -50; //meters
-    block->position = -50;
-    ball->mass = 1; //kg
-    block->mass = 50; //kg
-    block->velocity = 1; //meters per second
+    cout << "This will simulate 100 seconds of motion\nEnter starting distance from wall in m" << endl;
+    cin >> length;
+    ball->position = -1*length; //meters
+    block->position = -1*length;
+    cout << "Enter mass of Block and Ball in kg (separated by space)" << endl;
+    cin >> block->mass >> ball->mass;
+    cout << "Enter initial velocity in m/s"  << endl;
+    cin >> block->velocity;
+    
     ball->velocity = 0;
+    block->velocity = 0.001 * block->velocity; // meters per millisecond
     
-    
-    block->velocity = 0.001; // meters per millisecond
     collide(*block,*ball);
+    num_collisions = 1;
     
-    cout << "velocity: " << block->velocity << " " << ball->velocity << endl; //meters per millisecond
+    cout << "Block and Ball positions every 0.1 seconds" << endl;
 
-    for(int time = 0; time <= 1000000; time ++){ //time is 100,000 milliseconds, or 1000 seconds
+    for(int time = 1; time <= 100000; time++){ //100 seconds turned into millisecond step
+        
         movement(*ball);
         movement(*block);
         
  
-       
         if(approx_equal_pos(*ball,*wall) && (ball->velocity)) {
             hit_wall(*ball);
-            cout << "BOUNCE" << endl;
+            cout << "     BOUNCE" << endl;
         }
        
         if(approx_equal_pos(*block,*ball) && (ball->velocity < 0)){
             collide(*block,*ball);
-            cout << "COLLISION" << endl;
+            cout << "   COLLISION" << endl;
+            num_collisions++;
 
         }
         
         
-        if(time%1000 == 0)
-            cout << block->position << " " << ball->position << endl;
+        if(time%100 == 0)
+            cout << static_cast<double>(time)/1000 << ": " << block->position << " " << ball->position << endl;
         
-        if(block->velocity <0) {
-            cout << "END VELOCITY: " << block->velocity << endl;
+        if(block->velocity < 0) {
             cout << "END POSITION: " << block->position << endl;
-            cout << "Theoretical pos: " << -1 * length * sqrt(ball->mass/block->mass) << endl;
+            cout << "COLLISION COUNT: " << num_collisions << endl;
+            cout << "Stopped in " << static_cast<double>(time)/1000 << " seconds" << endl;
+            cout << "Theoretical Position: " << -1 * length * sqrt(ball->mass/block->mass) << endl;
+            cout << "Theoretical Collision #: " << 0.5*M_PI/atan(2*(sqrt(block->mass*ball->mass))/(block->mass - ball->mass)) << endl;
+            delete block;
+            delete ball;
             return 0;
         }
     }
-    cout << "velocity: " << block->velocity << " " << ball->velocity << endl; //meters per millisecond
 
 
     
